be1a93c76e8ba121e00ddcef34203acdb65c8b4a
geoffroy hely <geoffroyhely@gmail.com>
2016-12-08 14:37:43 +0100

    setup-blog-controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,60 @@
+class PostsController < ApplicationController
+  before_action :find_post, only: [:edit, :update, :show, :destroy]
+
+  # Index action to render all posts
+  def index
+    @posts = Post.all
+  end
+
+  # New action for creating post
+  def new
+    @post = Post.new
+  end
+
+  # Create action saves the post into database
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Successfully created post!"
+      redirect_to post_path(@post)
+    else
+      flash[:alert] = "Error creating new post!"
+      render :new
+    end
+  end
+
+  # Edit action retrives the post and renders the edit page
+  def edit
+  end
+
+  # Update action updates the post with the new information
+  def update
+    if @post.update(post_params)
+      flash[:notice] = "Successfully updated post!"
+      redirect_to post_path(@post)
+    else
+      flash[:alert] = "Error updating post!"
+      render :edit
+    end
+  end
+
+  # The show action renders the individual post after retrieving the the id
+  def show
+  end
+
+  # The destroy action removes the post permanently from the database
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :photo, :photo_cache)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+end
